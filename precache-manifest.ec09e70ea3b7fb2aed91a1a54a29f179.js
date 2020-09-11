83dcf9bae94224a016fbf369ce5676a21c1f434c
Waithera Mwaura <ceramwaura19@gmail.com>
2020-09-11 20:48:38 +0300

    Updates

---
+++ precache-manifest.ec09e70ea3b7fb2aed91a1a54a29f179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4698ae1705654b07efbf0fe487c8c517",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3d0010f5c8906be52d66",
+    "url": "/my-app/static/js/2.d3400bd3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.d3400bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b619ef2ba86a5c73cb8d",
+    "url": "/my-app/static/js/main.eb07205a.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
